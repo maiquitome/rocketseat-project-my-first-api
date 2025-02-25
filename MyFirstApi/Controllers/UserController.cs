a8bddc2104dc0792153225af5387973df83c2715
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-02-25 07:27:41 -0300

    feat: :sparkles: add `delete endpoint` to `UserController`

--- MyFirstApi/Controllers/UserController.cs
+++ MyFirstApi/Controllers/UserController.cs
@@ -46,4 +46,12 @@ public class UserController : ControllerBase
     {
         return NoContent();
     }
+
+    [HttpDelete]
+    // [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Delete(/* [FromRoute] int id */)
+    {
+        return NoContent();
+    }
 }
